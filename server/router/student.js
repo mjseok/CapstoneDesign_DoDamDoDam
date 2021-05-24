7e606c71168269b686bc86a89d105c8d1a433df3
rlqoznmm <rlqoznmm@naver.com>
2021-05-24 19:57:07 +0900

    스펠링 체크 부산대 버전으로 변경

--- server/router/student.js
+++ server/router/student.js
@@ -1,11 +1,11 @@
-const hanspell = require('hanspell');
-const db = require('../models');
-const router = require('express').Router();
-const { wrapper, CustomError, FormError } = require('./error');
-const { isEmpty } = require('lodash');
+const hanspell = require("hanspell");
+const db = require("../models");
+const router = require("express").Router();
+const { wrapper, CustomError, FormError } = require("./error");
+const { isEmpty } = require("lodash");
 
 exports.studentSpellCheck = router.get(
-  '/student/spell',
+  "/student/spell",
   wrapper(async (req, res) => {
     try {
       const text = req.query.text;
@@ -18,15 +18,15 @@ exports.studentSpellCheck = router.get(
           resolve(checkedSpell);
         };
         const error = function (err) {
-          console.error('spell check error: ' + err);
+          console.error("spell check error: " + err);
           resolve(checkedSpell);
         };
-        hanspell.spellCheckByDAUM(text, 6000, getResult, end, error);
+        hanspell.spellCheckByPNU(text, 6000, getResult, end, error);
       });
       res.status(200).send(corrected);
     } catch (err) {
       console.error(err);
-      throw new CustomError({ code: 500, message: '맞춤법 검사에 오류가 발생했습니다.' });
+      throw new CustomError({ code: 500, message: "맞춤법 검사에 오류가 발생했습니다." });
     }
   })
 );
