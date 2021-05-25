210cb6b3adef3ac6c88fde1b7e190e847352b9cc
sawon choi <sawon49@naver.com>
2021-05-25 15:17:36 +0900

    일기 명사 추출 후 mysql 업데이트 및 저장

--- server/mysql_connection.py
+++ server/mysql_connection.py
@@ -6,6 +6,10 @@ import MorphemeSeparation as ms
 
 import pymysql
 
+import datetime
+
+days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
 '''
 conn = pymysql.connect(host='localhost', user='root', password='0000', charset='utf8', db='database_production') #DB local 연결
 '''
@@ -66,10 +70,46 @@ def getData(index) :
     return cur.fetchone()
 
 
-def updateWordCloud():
+def updateWordCloud(diary, teacher_id, day):
+    conn = pymysql.connect(host='115.85.181.160', port=3306, user='root', password='0000', charset='utf8', db='database_production')
+    words = ms.count_word_freq(diary)
     
+    for tup in words :
+        query = "SELECT * FROM wordclouds where teacher_id = %s and word = %s"
+        cur = conn.cursor()
+        cur.execute(query, (teacher_id, tup[0]))
+        line = cur.fetchone()
+        cur.close()
+        
+        count = int(tup[1])
+        
+        if line is None :
+            query = """INSERT INTO wordclouds (teacher_id, word)
+            VALUES (%s, %s)
+            """
+            
+            cur = conn.cursor()
+            cur.execute(query, (teacher_id, tup[0]))
+            cur.close()
+            conn.commit()
+            
+        else :
+            count = count + line[3+day]
+            
+
+        query = " UPDATE wordclouds set " + str(days[day]) + " = %s where word = %s and teacher_id = %s "
+            
+        cur = conn.cursor()
+        
+        cur.execute(query, (count, tup[0], teacher_id))
+        conn.commit()
+        cur.close()
+            
+    conn.close()
 
 def run(index) :
     data = getData(index)
     updateSentimentAnalysis(index, data[4])
-    
+    current_time = datetime.datetime.now()
+    updateWordCloud(data[4], data[2], int(current_time.weekday()))
+
