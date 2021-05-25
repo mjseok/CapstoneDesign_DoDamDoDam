3a1b4c97f61af8ace38698c2d444494f4fe641a4
sawon choi <sawon49@naver.com>
2021-05-25 15:41:27 +0900

    단어 초기화 코드 에러 제거

--- server/mysql_day_reset.py
+++ server/mysql_day_reset.py
@@ -1,4 +1,4 @@
-
+#-*- coding: utf-8 -*-
 import datetime
 
 import pymysql
@@ -7,11 +7,11 @@ days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 
 
 current_time = datetime.datetime.now()
-yesterday = (6+int(current_time.weekday()))%7
+today = int(current_time.weekday())
 
 conn = pymysql.connect(host='115.85.181.160', port=3306, user='root', password='0000', charset='utf8', db='database_production')
 
-sql = "update wordclouds set " + days[yesterday] + " = '0' "
+sql = "update wordclouds set " + days[today] + " = '0' "
 
 cur = conn.cursor()
 
