90aca9de165d1f432f513ffc0bf8395c5a0a8352
mjseok <mjseok98@gmail.com>
2021-06-18 12:12:52 +0900

    사용하지 않는 코드 삭제

--- server/dbquery/teacherQuery.js
+++
@@ -1,7 +0,0 @@
-exports.selectJournalTeacher = `SELECT contents, happy, neutral, fear, anger, sadness, main_emotion FROM journal WHERE student_id=? AND date=?`;
-exports.insertComment = `UPDATE journal SET comment=? WHERE student_id=? AND date=?`;
-exports.selectAllStudent = `SELECT name, number, photo FROM student WHERE teacher_id=?`;
-exports.selectStudent = `SELECT * FROM student WHERE id=?`;
-exports.insertStudent = `INSERT INTO student(id, password, teacher_id, number, birth, photo) VALUES(?,?,?,?,?,?)`;
-exports.deleteStudent = `DELETE FROM student WHERE id=?`;
-exports.updateStudent = `UPDATE student SET teacher_id=?, number=?, birth=?, photo=? WHERE id=?`;
